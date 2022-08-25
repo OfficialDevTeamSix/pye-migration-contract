5b7077dc615dde2a9e1056719e997c62bc3f4cb6
Tommy K <tommy@devteamsix.com>
2022-08-25 02:15:05 -0700

    Updates

--- .deps/npm/@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
+++ .deps/npm/@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
@@ -1,26 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+// OpenZeppelin Contracts (last updated v4.7.0) (token/ERC20/utils/SafeERC20.sol)
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "../interfaces/IBEP20.sol";
+pragma solidity ^0.8.0;
 
-// 
+import "../IERC20.sol";
+import "../extensions/draft-IERC20Permit.sol";
+import "../../../utils/Address.sol";
+
 /**
- * @title SafeBEP20
- * @dev Wrappers around BEP20 operations that throw on failure (when the token
+ * @title SafeERC20
+ * @dev Wrappers around ERC20 operations that throw on failure (when the token
  * contract returns false). Tokens that return no value (and instead revert or
  * throw on failure) are also supported, non-reverting calls are assumed to be
  * successful.
- * To use this library you can add a `using SafeBEP20 for IBEP20;` statement to your contract,
+ * To use this library you can add a `using SafeERC20 for IERC20;` statement to your contract,
  * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
  */
-library SafeBEP20 {
-    using SafeMath for uint256;
+library SafeERC20 {
     using Address for address;
 
     function safeTransfer(
-        IBEP20 token,
+        IERC20 token,
         address to,
         uint256 value
     ) internal {
@@ -28,7 +28,7 @@ library SafeBEP20 {
     }
 
     function safeTransferFrom(
-        IBEP20 token,
+        IERC20 token,
         address from,
         address to,
         uint256 value
@@ -38,46 +38,62 @@ library SafeBEP20 {
 
     /**
      * @dev Deprecated. This function has issues similar to the ones found in
-     * {IBEP20-approve}, and its usage is discouraged.
+     * {IERC20-approve}, and its usage is discouraged.
      *
      * Whenever possible, use {safeIncreaseAllowance} and
      * {safeDecreaseAllowance} instead.
      */
     function safeApprove(
-        IBEP20 token,
+        IERC20 token,
         address spender,
         uint256 value
     ) internal {
         // safeApprove should only be called when setting an initial allowance,
         // or when resetting it to zero. To increase and decrease it, use
         // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
-        // solhint-disable-next-line max-line-length
         require(
             (value == 0) || (token.allowance(address(this), spender) == 0),
-            "SafeBEP20: approve from non-zero to non-zero allowance"
+            "SafeERC20: approve from non-zero to non-zero allowance"
         );
         _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
     }
 
     function safeIncreaseAllowance(
-        IBEP20 token,
+        IERC20 token,
         address spender,
         uint256 value
     ) internal {
-        uint256 newAllowance = token.allowance(address(this), spender).add(value);
+        uint256 newAllowance = token.allowance(address(this), spender) + value;
         _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
     }
 
     function safeDecreaseAllowance(
-        IBEP20 token,
+        IERC20 token,
         address spender,
         uint256 value
     ) internal {
-        uint256 newAllowance = token.allowance(address(this), spender).sub(
-            value,
-            "SafeBEP20: decreased allowance below zero"
-        );
-        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+        unchecked {
+            uint256 oldAllowance = token.allowance(address(this), spender);
+            require(oldAllowance >= value, "SafeERC20: decreased allowance below zero");
+            uint256 newAllowance = oldAllowance - value;
+            _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+        }
+    }
+
+    function safePermit(
+        IERC20Permit token,
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) internal {
+        uint256 nonceBefore = token.nonces(owner);
+        token.permit(owner, spender, value, deadline, v, r, s);
+        uint256 nonceAfter = token.nonces(owner);
+        require(nonceAfter == nonceBefore + 1, "SafeERC20: permit did not succeed");
     }
 
     /**
@@ -86,16 +102,15 @@ library SafeBEP20 {
      * @param token The token targeted by the call.
      * @param data The call data (encoded using abi.encode or one of its variants).
      */
-    function _callOptionalReturn(IBEP20 token, bytes memory data) private {
+    function _callOptionalReturn(IERC20 token, bytes memory data) private {
         // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
         // we're implementing it ourselves. We use {Address.functionCall} to perform this call, which verifies that
         // the target address contains contract code and also asserts for success in the low-level call.
 
-        bytes memory returndata = address(token).functionCall(data, "SafeBEP20: low-level call failed");
+        bytes memory returndata = address(token).functionCall(data, "SafeERC20: low-level call failed");
         if (returndata.length > 0) {
             // Return data is optional
-            // solhint-disable-next-line max-line-length
-            require(abi.decode(returndata, (bool)), "SafeBEP20: BEP20 operation did not succeed");
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
         }
     }
 }
--- contracts/MultiMigration.sol
+++ contracts/MultiMigration.sol
@@ -1,147 +1,83 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./libraries/SafeBEP20.sol";
+import "./libraries/SafeERC20.sol";
 
 contract MultiMigration is Ownable, ReentrancyGuard {
-    using SafeBEP20 for IBEP20;
+    using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
     uint256 accuracyFactor = 10**18;
     uint256 divisor = 10**18;
 
     address public immutable oldPYE = 0x4d542De559D9696cbC15a3937Bf5c89fEdb5b9c7;
-    address public immutable oldMINI = 0xBa07EED3d09055d60CAEf2bDfCa1c05792f2dFad;
     address public immutable oldFORCE = 0xEcE3D017A62b8723F3648a9Fa7cD92f603E88a0E;
-    address public immutable oldBULL = 0xA354F2185f8240b04f3f3C7b56A8Cd66F00b58db;
-    
     address public immutable oldAPPLE = 0x5a83d81daCDcd3f5a5A712823FA4e92275d8ae9F;
-    address public immutable oldTRICK = 0xE5F8Ea8A9081f7CaBb9D155Fb12B599E30c32AFE;
-    address public immutable oldFUEL = 0xc4a21f59628c82Ff916F2267ad97f250A572DB4b;
-    address public immutable oldRIDE = 0x10A0ddd99ff720BE0f247995d0E43CbaBd14D466;
-
     address public immutable oldCHERRY = 0xc1D6A3ef07C6731DA7FDE4C54C058cD6e371dA04;
-    address public immutable oldTREAT = 0x4c091d0cbdCaF7eA2Bdd3beD7b0E787A95B9b483;
-    address public immutable oldGRAVITY = 0x8B9386354C6244232e44E03932f2484b37fB94E2;
-    address public immutable oldCHARGE = 0xA0340e9261C120708FD74b644A9Ff2D339B3eaee;
 
-    uint256 public pyeRate;
-    uint256 public miniRate;
-    uint256 public forceRate;
-    uint256 public bullRate;
+    uint256 public pyeRate = 125;
+    uint256 public forceRate = 200;
 
     address public newPYE;
     address public newAPPLE;
-    address public newPEACH;
     address public newCHERRY;
 
     address deadWallet = 0x000000000000000000000000000000000000dEaD;
 
-    event NewTokenTransfered(address indexed operator, IBEP20 newToken, uint256 sendAmount);
+    event NewTokenTransfered(address indexed operator, IERC20 newToken, uint256 sendAmount);
 
     // update migrate info    
-    function setConversionRates(uint256 _pyeRate, uint256 _miniRate, uint256 _forceRate, uint256 _bullRate) external onlyOwner{    
+    function setConversionRates(uint256 _pyeRate, uint256 _forceRate) external onlyOwner{    
         pyeRate = _pyeRate;
-        miniRate = _miniRate;
         forceRate = _forceRate;
-        bullRate = _bullRate;
     }
 
-    function setNewTokens(address _newPYE, address _newAPPLE, address _newPEACH, address _newCHERRY) external onlyOwner{
+    function setNewTokens(address _newPYE, address _newAPPLE, address _newCHERRY) external onlyOwner{
         newPYE = _newPYE;
         newAPPLE = _newAPPLE;
-        newPEACH = _newPEACH;
         newCHERRY = _newCHERRY;
     }
 
     function handlePYE(address account) internal {
-        uint256 PYE1;
-        uint256 PYE2;
-        uint256 PYE3;
-        uint256 PYE4;
+        uint256 newPYEAmount = 0;
 
-        uint256 oldPYEAmount = IBEP20(oldPYE).balanceOf(account);
-        uint256 oldMINIAmount = IBEP20(oldMINI).balanceOf(account);
-        uint256 oldFORCEAmount = IBEP20(oldFORCE).balanceOf(account);
-        uint256 oldBULLAmount = IBEP20(oldBULL).balanceOf(account);
+        uint256 oldPYEAmount = IERC20(oldPYE).balanceOf(account);
+        uint256 oldFORCEAmount = IERC20(oldFORCE).balanceOf(account);
 
         if(oldPYEAmount > 0) {
-            PYE1 = oldPYEAmount.mul(accuracyFactor).div(pyeRate).div(divisor);
-            IBEP20(oldPYE).safeTransferFrom(account, deadWallet, oldPYEAmount);
-        }
-        if(oldMINIAmount > 0) {
-            PYE2 = oldMINIAmount.mul(accuracyFactor).div(miniRate).div(divisor);
-            IBEP20(oldMINI).safeTransferFrom(account, deadWallet, oldMINIAmount);
+            newPYEAmount += oldPYEAmount.mul(accuracyFactor).div(pyeRate).div(divisor);
+            IERC20(oldPYE).safeTransferFrom(account, deadWallet, oldPYEAmount);
         }
         if(oldFORCEAmount > 0) {
-            PYE3 = oldFORCEAmount.mul(accuracyFactor).div(forceRate).div(divisor);
-            IBEP20(oldFORCE).safeTransferFrom(account, deadWallet, oldFORCEAmount);
+            newPYEAmount += oldFORCEAmount.mul(accuracyFactor).div(forceRate).div(divisor);
+            IERC20(oldFORCE).safeTransferFrom(account, deadWallet, oldFORCEAmount);
         }
-        if(oldBULLAmount > 0) {
-            PYE4 = oldBULLAmount.mul(accuracyFactor).div(bullRate).div(divisor);
-            IBEP20(oldBULL).safeTransferFrom(account, deadWallet, oldBULLAmount);
-        }
-        uint256 newPYEAmount = PYE1 + PYE2 + PYE3 + PYE4;
 
         if(newPYEAmount > 0) {
-            IBEP20(newPYE).safeTransfer(account, newPYEAmount);
-            emit NewTokenTransfered(account, IBEP20(newPYE), newPYEAmount); 
+            IERC20(newPYE).safeTransfer(account, newPYEAmount);
+            emit NewTokenTransfered(account, IERC20(newPYE), newPYEAmount); 
         }
     }
 
     function handleAPPLE(address account) internal {
-        uint256 oldAPPLEAmount = IBEP20(oldAPPLE).balanceOf(account);
-        uint256 oldTRICKAmount = IBEP20(oldTRICK).balanceOf(account);
-        uint256 oldFUELAmount = IBEP20(oldFUEL).balanceOf(account);
-        uint256 oldRIDEAmount = IBEP20(oldRIDE).balanceOf(account);
+        uint256 oldAPPLEAmount = IERC20(oldAPPLE).balanceOf(account);
 
         if(oldAPPLEAmount > 0) {
-            IBEP20(oldAPPLE).safeTransferFrom(account, deadWallet, oldAPPLEAmount);
-        }
-        if(oldTRICKAmount > 0) {
-            IBEP20(oldTRICK).safeTransferFrom(account, deadWallet, oldTRICKAmount);
-        }
-        if(oldFUELAmount > 0) {
-            IBEP20(oldFUEL).safeTransferFrom(account, deadWallet, oldFUELAmount);
-        }
-        if(oldRIDEAmount > 0) {
-            IBEP20(oldRIDE).safeTransferFrom(account, deadWallet, oldRIDEAmount);
-        }
-        uint256 newAPPLEAmount = oldAPPLEAmount + oldTRICKAmount + oldFUELAmount + oldRIDEAmount;
-
-        if(newAPPLEAmount > 0) {
-            IBEP20(newAPPLE).mint(account, newAPPLEAmount);
-            emit NewTokenTransfered(account, IBEP20(newAPPLE), newAPPLEAmount);  
+            IERC20(oldAPPLE).safeTransferFrom(account, deadWallet, oldAPPLEAmount);
+            IERC20(newAPPLE).mint(account, oldAPPLEAmount);
+            emit NewTokenTransfered(account, IERC20(newAPPLE), oldAPPLEAmount);  
         }
     }
 
-    function handlePEACHERRY(address account) internal {
-        uint256 oldCHERRYAmount = IBEP20(oldCHERRY).balanceOf(account);
-        uint256 oldTREATAmount = IBEP20(oldTREAT).balanceOf(account);
-        uint256 oldGRAVITYAmount = IBEP20(oldGRAVITY).balanceOf(account);
-        uint256 oldCHARGEAmount = IBEP20(oldCHARGE).balanceOf(account);
+    function handleCHERRY(address account) internal {
+        uint256 oldCHERRYAmount = IERC20(oldCHERRY).balanceOf(account);
 
         if(oldCHERRYAmount > 0) {
-            IBEP20(oldCHERRY).safeTransferFrom(account, deadWallet, oldCHERRYAmount);
-            IBEP20(newCHERRY).mint(account, oldCHERRYAmount);
-            emit NewTokenTransfered(account, IBEP20(newCHERRY), oldCHERRYAmount); 
-        }
-        if(oldTREATAmount > 0) {
-            IBEP20(oldTREAT).safeTransferFrom(account, deadWallet, oldTREATAmount);
-        }
-        if(oldGRAVITYAmount > 0) {
-            IBEP20(oldGRAVITY).safeTransferFrom(account, deadWallet, oldGRAVITYAmount);
-        }
-        if(oldCHARGEAmount > 0) {
-            IBEP20(oldCHARGE).safeTransferFrom(account, deadWallet, oldCHARGEAmount);
-        }
-        uint256 newPEACHAmount = oldTREATAmount + oldGRAVITYAmount + oldCHARGEAmount;
-
-        if(newPEACHAmount > 0) {
-            IBEP20(newPEACH).mint(account, newPEACHAmount);
-            emit NewTokenTransfered(account, IBEP20(newPEACH), newPEACHAmount);  
+            IERC20(oldCHERRY).safeTransferFrom(account, deadWallet, oldCHERRYAmount);
+            IERC20(newCHERRY).mint(account, oldCHERRYAmount);
+            emit NewTokenTransfered(account, IERC20(newCHERRY), oldCHERRYAmount); 
         }
     }
 
@@ -150,11 +86,11 @@ contract MultiMigration is Ownable, ReentrancyGuard {
         require(msg.sender != deadWallet, "Not allowed to dead wallet");
         handlePYE(msg.sender);
         handleAPPLE(msg.sender);
-        handlePEACHERRY(msg.sender);
+        handleCHERRY(msg.sender);
     }
 
     // Withdraw rest or wrong tokens that are sent here by mistake
-    function drainBEP20Token(IBEP20 token, uint256 amount, address to) external onlyOwner {
+    function drainBEP20Token(IERC20 token, uint256 amount, address to) external onlyOwner {
         if( token.balanceOf(address(this)) < amount ) {
             amount = token.balanceOf(address(this));
         }
